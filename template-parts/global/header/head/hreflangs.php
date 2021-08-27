f9d8cacb4d3681f32959f537ccb025a970a3a931
yurii.hnatyshyn <sylar11111@gmail.com>
2021-08-27 19:04:21 +0300

    Theme updating[2.3]: Fix from v2.2

--- template-parts/global/header/head/hreflangs.php
+++ template-parts/global/header/head/hreflangs.php
@@ -4,7 +4,7 @@ $id = $object->ID ?: $object->taxonomy . '_' .$object->term_id;
 $hreflangs = get_field('links_hreflang', $id);
 $current_hreflang = get_field('current_language_hreflang', 'option');
 ?>
-<link rel="alternate" hreflang="<?php echo $current_hreflang ?>" href="<?php echo get_stylesheet_directory_uri() . '/' ?>">
+<link rel="alternate" hreflang="<?php echo $current_hreflang ?>" href="<?php echo venom_get_current_url() ?>">
 <?php if ($hreflangs && count($hreflangs)) {
 	foreach ($hreflangs as $hreflang) {
 		$hreflang_code = $hreflang['domain'];
